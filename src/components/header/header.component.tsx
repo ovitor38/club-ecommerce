3463b67c3f91e0a6de3e8e9293393531d888083d
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-05-19 21:22:49 -0300

    feat(header): ensure login account creation and sign out are shown based on authentication state

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -7,9 +7,15 @@ import {
   HeaderTitle
 } from './header.style'
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { USerContext } from '../../context/user.context'
+import { auth } from '../../config/firebase.config'
+import { signOut } from 'firebase/auth'
 
 const Header = () => {
   const navigate = useNavigate()
+
+  const { isAuthenticated } = useContext(USerContext)
 
   const handleLoginClick = () => {
     navigate('/login')
@@ -25,8 +31,16 @@ const Header = () => {
 
       <HeaderItems>
         <HeaderItem>Explorar</HeaderItem>
-        <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
-        <HeaderItem onClick={handleSignUpClick}>Criar Conta</HeaderItem>
+        {!isAuthenticated && (
+          <>
+            <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
+            <HeaderItem onClick={handleSignUpClick}>Criar Conta</HeaderItem>
+          </>
+        )}
+
+        {isAuthenticated && (
+          <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
+        )}
         <HeaderItem>
           <BsCart3 size={25} />
           <p style={{ marginLeft: 5 }}>5</p>
