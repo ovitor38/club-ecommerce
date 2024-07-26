8798dae64c716e6355d516e23e6a88d8762423ac
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-07-26 11:31:57 -0300

    feat(cart): add cart context

---
+++ src/context/cart.context.tsx
@@ -0,0 +1,32 @@
+import { createContext, FunctionComponent, useState } from 'react'
+import { ICartProduct } from '../types/cart.types'
+import { ICommonProps } from '../common/interfaces'
+
+export interface ICartContext {
+  isVisible: boolean
+  products: ICartProduct[]
+  toogleCart: () => void
+}
+
+const CartContext = createContext<ICartContext>({
+  isVisible: false,
+  products: [],
+  toogleCart: () => {}
+})
+
+const CartContextProvider: FunctionComponent<ICommonProps> = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [products] = useState<ICartProduct[]>([])
+
+  const toogleCart = () => {
+    setIsVisible((prevState) => !prevState)
+  }
+
+  return (
+    <CartContext.Provider value={{ isVisible, products, toogleCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartContextProvider
--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import UserContextprovider from './context/user.context'
 import CategoryContextProvider from './context/category.context'
+import CartContextProvider from './context/cart.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <UserContextprovider>
       <CategoryContextProvider>
-        <App />
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
       </CategoryContextProvider>
     </UserContextprovider>
   </React.StrictMode>
