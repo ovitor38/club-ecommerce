2eb3f2e19eb043ca3661e0957b73101ca956e591
Victor Oliveira <victor.amorim@ntconsult.com.br>
2024-08-19 10:53:26 -0300

    chore(redux): add redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,21 @@ import reportWebVitals from './reportWebVitals'
 import UserContextprovider from './context/user.context'
 import CategoryContextProvider from './context/category.context'
 import CartContextProvider from './context/cart.context'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <UserContextprovider>
-      <CategoryContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </CategoryContextProvider>
-    </UserContextprovider>
+    <Provider store={store}>
+      <UserContextprovider>
+        <CategoryContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </CategoryContextProvider>
+      </UserContextprovider>
+    </Provider>
   </React.StrictMode>
 )
 
